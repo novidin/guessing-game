82711eb77fc3c111f454a75f3aadebcdef38987e
Di <novidin@mail.ru>
2022-08-10 11:08:32 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result = 0;
+        this.min = 0;
+        this.max = 0;
+     }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = this.min + Math.ceil((this.max - this.min) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
 module.exports = GuessingGame;
+
