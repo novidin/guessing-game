0c6eb869f4d27bc2722ab01a81f380fb545057bb
novidin <novidin@mail.ru>
2022-10-26 18:59:12 +0300

    Fix css

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    //this.result = 0;
+    this.array = [];
+    this.index = 0;
+  }
 
-    setRange(min, max) {
-
+  setRange(min, max) {
+    // this.min = min;
+    // this.max = max;
+    for(let i = 0; i < max; i++) {
+      this.array.push(i)
     }
+    this.max = this.array.length;
+  }
 
-    guess() {
+  guess() {
+    this.index = Math.ceil((this.min + this.max) / 2)
+    return this.array[this.index];
+  }
 
-    }
+  lower() {
+    this.max = this.index - 1;
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.index;
+  }
 }
 
 module.exports = GuessingGame;
